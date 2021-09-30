8995d9c6c4114cfa236bc21e4f768b4a4ca48c8b
fullstacksk <fullstacksk@gmail.com>
2021-09-30 11:28:57 +0530

    getting feeds of following users

--- components/main/Feeds.js
+++ components/main/Feeds.js
@@ -1,24 +1,98 @@
-import React from 'react';
-import { FlatList, View, StyleSheet, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Image, Button, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import Post from './Post';
+import 'firebase/firestore';
 
-const Feeds = ({ posts }) => {
-	console.log('All Posts : ', posts);
+const Feeds = ({ users, usersLoaded, userFollowing, navigation, route }) => {
+	const [ posts, setPosts ] = useState([]);
+
+	useEffect(
+		() => {
+			let posts = [];
+			if (usersLoaded === userFollowing.length) {
+				userFollowing.forEach((uid) => {
+					const user = users.find((el) => el.uid === uid);
+					if (user !== undefined) {
+						posts = [ ...posts, ...user.posts ];
+					}
+				});
+			}
+			posts.sort((x, y) => {
+				return x.createdAt - y.createdAT;
+			});
+			setPosts(posts);
+		},
+		[ usersLoaded ]
+	);
+
+	// if (user === null) return <View />;
 	return (
 		<View style={styles.container}>
-			<FlatList numColumns={1} horizontal={false} data={posts} renderItem={({ item }) => <Post post={item} />} />
+			{!posts.length && <Text style={styles.textCenter}>Follow People for feeds</Text>}
+			<View style={styles.container}>
+				<FlatList
+					numColumns={1}
+					horizontal={false}
+					data={posts}
+					renderItem={({ item }) => <Post post={item} />}
+				/>
+			</View>
 		</View>
 	);
 };
 
 const mapStateToProps = (store) => ({
-	posts: store.userState.posts
+	currentUser: store.userState.currentUser,
+	userFollowing: store.userState.following,
+	users: store.usersState.users,
+	usersLoaded: store.usersState.usersLoaded
 });
 export default connect(mapStateToProps)(Feeds);
+
 const styles = StyleSheet.create({
 	container: {
+		flex: 1
+	},
+	acountDetail: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginBottom: 24,
+		padding: 16
+	},
+	text: {
+		marginBottom: 8,
+		fontWeight: '600'
+	},
+	textCenter: {
+		marginBottom: 8,
+		fontWeight: '600',
+		textAlign: 'center'
+	},
+	avatar: {
+		// flex:1/3,
+		width: 100,
+		height: 100,
+		borderRadius: 100 / 2
+	},
+	detail: {
+		flexGrow: 1,
+		justifyContent: 'center'
+	},
+	imageGallery: {
+		flex: 1
+	},
+	imageContainer: {
+		flex: 1 / 3
+	},
+	image: {
 		flex: 1,
-		backgroundColor: '#fff'
+		aspectRatio: 1 / 1,
+		height: 100
+	},
+	logoutButton: {
+		padding: 4,
+		backgroundColor: 'purple'
 	}
 });
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,5 +1,11 @@
 import firebase from 'firebase';
-import { USER_FOLLOWING_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_STATE_CHANGE } from '../constants';
+import {
+	USERS_DATA_STATE_CHANGE,
+	USERS_POSTS_STATE_CHANGE,
+	USER_FOLLOWING_STATE_CHANGE,
+	USER_POSTS_STATE_CHANGE,
+	USER_STATE_CHANGE
+} from '../constants';
 
 //Fetching User
 export const fetchUser = () => {
@@ -7,7 +13,6 @@ export const fetchUser = () => {
 		try {
 			const snapshot = await firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).get();
 			if (snapshot.exists) {
-				console.log('userSnapshot', snapshot.data());
 				dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() });
 			} else {
 				throw new Error('User not found');
@@ -60,7 +65,60 @@ export const fetchUserFollowing = () => {
 					});
 					console.log('following : ', following);
 					dispatch({ type: USER_FOLLOWING_STATE_CHANGE, following });
+					following.forEach((user) => dispatch(fetchUsersData(user)));
 				});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+};
+
+//fetching users data
+
+export const fetchUsersData = (uid) => {
+	return async (dispatch, getState) => {
+		const found = getState().usersState.users.some((el) => el.uid === uid);
+		if (!found) {
+			const snapshot = await firebase.firestore().collection('users').doc(uid).get();
+			if (snapshot.exists) {
+				let user = snapshot.data();
+				user.uid = snapshot.id;
+				await dispatch({ type: USERS_DATA_STATE_CHANGE, user });
+				await dispatch(fetchUserFollowingPosts(user.uid));
+			} else {
+				throw new Error('User not found');
+			}
+		}
+	};
+};
+
+//Fetching user following posts
+export const fetchUserFollowingPosts = (uid) => {
+	return async (dispatch, getState) => {
+		try {
+			const snapshot = await firebase
+				.firestore()
+				.collection('posts')
+				.doc(uid)
+				.collection('userPosts')
+				.orderBy('createdAt', 'desc')
+				.get();
+			if (snapshot.docs.length) {
+				const uid = snapshot.docs[0].ref.path.split('/')[1];
+				const user = getState().usersState.users.find((el) => el.uid === uid);
+				const posts = snapshot.docs.map((doc) => {
+					const data = doc.data();
+					const id = doc.id;
+					return { id, ...data, user };
+				});
+
+				console.log('fetchUserFollowingPosts : ', { user, posts });
+				dispatch({ type: USERS_POSTS_STATE_CHANGE, posts, uid });
+			} else {
+				throw new Error('Post not found');
+			}
+
+			console.log('getState : ', getState());
 		} catch (error) {
 			console.log(error);
 		}
